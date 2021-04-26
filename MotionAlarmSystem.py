bb3f56635877f7e5c218776b7703822da7b35f13
Daelyn Weaver <dew037@latech.edu>
2021-04-26 09:38:52 -0500

    began building out sample function

--- MotionAlarmSystem.py
+++ MotionAlarmSystem.py
@@ -12,4 +12,5 @@ MIXER_BUFF = 1024
 class Note(pygame.mixer.sound):
     def __init__(self, frequency, volume):
         self.freqeuncy = frequency
+        pygame.mixer.Sound.__init__(self, buffer = self.build.samples())
 
